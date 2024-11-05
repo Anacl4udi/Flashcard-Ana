b812ebe726fe003161bd3ac1487c88c198c98eea
Ana <cladiaana2008@gmail.com>
2024-11-05 09:56:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+   'Acessório',
+    'Qual a melhor opção de correntinha para usar com gola alta?',
+    '☆ A melhor correntinha para usar com roupas gola alta, são as mais cumpridas☆'
+)
+
+criaCartao(
+    'Unhas',
+    'Qual esmalte mais combina com o Natal?',
+    '☆ A capital da França é Paris ☆'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    '☆ A capital da França é Paris ☆'
+)
