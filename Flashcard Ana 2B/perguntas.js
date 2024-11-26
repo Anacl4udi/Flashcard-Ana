0082e229a3eb2d37f5a902418414643258f30fad
Ana <cladiaana2008@gmail.com>
2024-11-26 09:36:17 -0300

    Add files via upload

---
+++ Flashcard Ana 2B/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Acessório',
+     'Qual a melhor opção de correntinha para usar com gola alta?',
+     '☆ A melhor correntinha para usar com roupas gola alta, são as mais cumpridas☆'
+ )
+ 
+ criaCartao(
+     'Unhas',
+     'Qual esmalte mais combina com o Natal?',
+     '☆ O que mais combina com o natal é o Rubi da Risque☆'
+ )
+ 
+ criaCartao(
+     'Roupa',
+     'Qual a melhor marca de roupa feminina para ir para a praia?',
+     '☆ FARM é a melhor escolha para se vestir em dias quentes ☆'
+ )
+ criaCartao(
+    'Cabelo',
+     'O que fazer quando o cabelo está ressecado?',
+     '☆ Use a linha Divine Braé ☆'
+ )
+ 
+ criaCartao(
+     'Viagem',
+     'Para onde ir, quando se espera um lugar bem frio e com neve?',
+     '☆ A melhor opção é a Suíça☆'
+ )
+ 
+ criaCartao(
+     'Bolsa',
+     'Quais as melhores marcas de bolsas?',
+     '☆ As melhores marcas são Chanel, Louis Vuitton e Gucci ☆'
+ )
